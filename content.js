5976aa704848c2a5faac703dedc5b117911bfa75
hato810424 <yukkuri.hatopoppo@gmail.com>
2022-06-07 16:52:29 +0900

    レスポンスを真摯に受け取ってくれるようにした

--- content.js
+++ content.js
@@ -9,23 +9,31 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     })
     .then(response => response.json())
     .then(data => {
-        if(request.url.toLowerCase().startsWith('https://')) {
-            navigator.clipboard.writeText(data.url);
-        } else {
-            const inputID = 'shortURL';
-            if(document.querySelector(`#${inputID}`) === null) {
-                const $input = document.createElement('input');
-                $input.setAttribute('type', 'text');
-                $input.setAttribute('id', inputID);
-                $input.setAttribute('style', 'position: absolute; left: -100vw; top: 0');
-                document.body.appendChild($input);
+        if (data.response == 100, 101) {
+            if (request.url.toLowerCase().startsWith('https://')) {
+                navigator.clipboard.writeText(data.url);
+            } else {
+                const inputID = 'shortURL';
+                if (document.querySelector(`#${inputID}`) === null) {
+                    const $input = document.createElement('input');
+                    $input.setAttribute('type', 'text');
+                    $input.setAttribute('id', inputID);
+                    $input.setAttribute('style', 'position: absolute; left: -100vw; top: 0');
+                    document.body.appendChild($input);
+                }
+                const $inputDom = document.querySelector(`#${inputID}`);
+                $inputDom.value = data.url;
+                $inputDom.select();
+                document.execCommand('copy');
             }
-            const $inputDom = document.querySelector(`#${inputID}`);
-            $inputDom.value = data.url;
-            $inputDom.select();
-            document.execCommand('copy');
+            alert("成功！！\nURLをクリップボードにコピーしました！")
+        } else {
+            if (data.code == "timeout") {
+                alert("タイムアウトしました\n生成頻度が高すぎます！！")
+            } else {
+                alert("失敗しました\nしばらく時間をおいて再度お試しください")
+            }
         }
-         alert("クリップボードにコピーしました")
     }) 
     sendResponse({
         value: request.url,
